d43ebe061e162871dd777928af75c4bb780ae020
Hiro Protagonist <shdesai118@gmail.com>
2021-11-27 13:05:44 -0700

    added code for model relationships

--- models/index.js
+++ models/index.js
@@ -1,2 +1,32 @@
 // Require all the model files
 // This file is where you write your relationships between the models
+
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+// User has many Post
+User.hasMany(Post, {
+    foreignKey: 'creator_id',
+    onDelete: 'CASCADE',
+})
+
+// Post belongs to user
+Post.belongsTo(User, {
+    foreignKey: 'creator_id',
+    onDelete: 'CASCADE',
+})
+// Post can have many comments
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE',
+})
+
+// comment can belong to one post and one user
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE',
+})
+
+module.exports = { User, Post, Comment };
