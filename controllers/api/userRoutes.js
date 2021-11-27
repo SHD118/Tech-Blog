1662ef112c2a94007608cc84fb71884aaaa27562
Hiro Protagonist <shdesai118@gmail.com>
2021-11-27 13:25:19 -0700

    added code for userroute

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,7 +1,24 @@
 const router = require("express").Router();
-const { User } = require('../models');
+const { User } = require('../../models');
 
 // login in
+
+router.post('/login', async (req, res) => {
+    try {
+        const userNameData = await User.findOne({
+            where: {
+                email: req.body.email,
+            }
+        })
+        if (!userNameData){
+            res.status(404).json({ message: 'No user found with this email!' });
+            return;
+          }
+            
+    } catch {
+        
+    }
+})
 
 // logout
 
