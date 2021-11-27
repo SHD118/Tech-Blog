a9b93c4481364953e46df4435cd5b8afd146afe4
Hiro Protagonist <shdesai118@gmail.com>
2021-11-27 13:36:48 -0700

    added code forapi/index

---
+++ controllers/api/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+const userRoutes = require('./userRoutes');
+const userRoutes = require('./postRoutes');
+
+router.use('/users', userRoutes);
+// router.use('/post', postRoutes);
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router()
 const homeroutes = require("./homeroutes")
-const postroutes = require("./postroutes")
+// const postroutes = require("./api/postRoutes")
 router.use("/", homeroutes)
-router.use("/post", postroutes)
+// router.use("/post", postroutes)
 module.exports = router
